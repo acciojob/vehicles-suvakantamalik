f1c1a862436abbff1212d86610709fdb988939a6
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-12-19 10:12:00 +0530

    "solved"

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,20 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,6 +3,7 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+        super(name, 4, 4, 6, isManual,"Sports Car", 2);
         //Use arbitrary values for parameters which are not mentioned
     }
 
@@ -20,8 +21,23 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
+        else if(newSpeed <= 50){
+            changeGear(1);
+        }else if(newSpeed <= 100){
+            changeGear(2);
+        }else if(newSpeed <= 150){
+            changeGear(3);
+        }else if(newSpeed <= 200){
+            changeGear(4);
+        }else if(newSpeed <= 250){
+            changeGear(5);
+        }else if(newSpeed <= 300){
+            changeGear(6);
+        }
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
